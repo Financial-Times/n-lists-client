171f8c525eba8415853c09d382e25be77b576843
Matt Hinchliffe <matt@maketea.co.uk>
2018-02-27 22:33:21 +0000

    Do a little typings dance to allow 'for' to be a valid method name  🐿 v2.7.0

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,23 @@
-declare module '@financial-times/n-lists-client' {
-	interface Options {
-		fields: string[]
-	}
-
-	interface Response {
-		id: string,
-		title: string,
-		apiUrl: string,
-		concept?: {
-			id: string,
-			apiUrl: string,
-			prefLabel: string
-		},
-		listType?: string,
-		items: Array<{ id: string, apiUrl: string }>
-	}
-
-	export interface Static {
-		get(
-			listID: string,
-			options: Options,
-			timeout: number
-		): Promise<Response>;
-
-		for(
-			listType: string,
-			conceptID: string,
-			options: Options,
-			timeout: number
-		): Promise<Response>;
-	}
+interface Options {
+	fields: string[]
 }
+
+interface Response {
+	id: string,
+	title: string,
+	apiUrl: string,
+	concept?: {
+		id: string,
+		apiUrl: string,
+		prefLabel: string
+	},
+	listType?: string,
+	items: Array<{ id: string, apiUrl: string }>
+}
+
+declare const static: {
+	get(listID: string, options?: Options, timeout?: number): Promise<Response>;
+	for(listType: string, conceptID: string, options?: Options, timeout?: number): Promise<Response>;
+}
+
+export = static;
